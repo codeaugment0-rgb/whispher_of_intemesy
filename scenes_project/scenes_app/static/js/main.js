3a7bc19c5566a4c1a4d5af86bcbf09a8dcc30188
codeaugment0-rgb <code.augment.0@gmail.com>
2025-08-16 09:23:37 +0530

    asdh

--- scenes_project/scenes_app/static/js/main.js
+++ scenes_project/scenes_app/static/js/main.js
@@ -84,10 +84,10 @@
 
       // Show success state
       button.innerHTML = `
-        <svg class="w-4 h-4 mr-2" fill="currentColor" viewBox="0 0 20 20">
+        <svg class="w-4 h-4 mr-2 text-gray-700" viewBox="0 0 20 20">
           <path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path>
         </svg>
-        <span class="hidden lg:inline">Copied!</span>
+        <span class="text-gray-700 hidden lg:inline">Copied!</span>
       `;
       button.classList.add('text-gray-100');
 
